22157b8427ad9204e8fceba9dbab4fda04400f70
davidvazgon26 <davidvazgon@hotmail.com>
2022-03-12 23:50:10 -0600

    Creacion de esquemas de DD

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4  //Generacion aut de id
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image:{
+      type: DataTypes.TEXT,
+      validate:{isUrl:true},
+      defaultValue: 'https://i.pinimg.com/originals/76/c6/46/76c64646e9be7aef25124d07b20aa5e2.jpg'
+    },
+    // clientGame:{
+      //   type: DataTypes.BOOLEAN,
+      //   defaultValue: false
+      // }
+      timestamps: false, 
+      createdAt: false,
+      
+    });
 };
